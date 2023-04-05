89254d2ba2a4b5df68ad3b67fe3c20c173c6f418
Junaid <jshah0168@gmail.com>
2023-04-05 15:57:15 +0530

    commit 1

---
+++ randomNumber.java
@@ -0,0 +1,10 @@
+import java.util.Random;
+
+public class randomNumber {
+   public static void main(String[] args) {
+      Random random = new Random();
+      for (int i = 0; i < 10; i++) {
+         System.out.print(random.nextInt(100) + " ");
+      }
+   }
+}
